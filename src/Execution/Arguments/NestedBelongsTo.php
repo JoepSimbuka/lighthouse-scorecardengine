23af936bd70d69ec259fd6b6e730fbc695123632
JoepvdBroek <joep.vandenbroek@simbuka.com>
2020-05-13 13:34:55 +0200

    adjusted code to make mutations work for with our code

--- src/Execution/Arguments/NestedBelongsTo.php
+++ src/Execution/Arguments/NestedBelongsTo.php
@@ -7,78 +7,53 @@ use Nuwave\Lighthouse\Support\Contracts\ArgResolver;
 
 class NestedBelongsTo implements ArgResolver
 {
-    /**
-     * @var \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    protected $relation;
+	/**
+	 * @var \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
+	protected $relation;
 
-    public function __construct(BelongsTo $relation)
-    {
-        $this->relation = $relation;
-    }
+	public function __construct(BelongsTo $relation)
+	{
+		$this->relation = $relation;
+	}
 
-    /**
-     * @param  \Illuminate\Database\Eloquent\Model  $parent
-     * @param  \Nuwave\Lighthouse\Execution\Arguments\ArgumentSet  $args
-     */
-    public function __invoke($parent, $args): void
-    {
-        if ($args->has('create')) {
-            $saveModel = new ResolveNested(new SaveModel($this->relation));
+	/**
+	 * @param  \Illuminate\Database\Eloquent\Model  $parent
+	 * @param  \Nuwave\Lighthouse\Execution\Arguments\ArgumentSet  $args
+	 */
+	public function __invoke($parent, $args): void
+	{
+		$reflection = new \ReflectionClass($this->relation->getRelated());
+		$availability = $reflection->getStaticPropertyValue('AVAILABILITY_IN_GRAPHQL');
 
-            $related = $saveModel(
-                $this->relation->make(),
-                $args->arguments['create']->value
-            );
-            $this->relation->associate($related);
-        }
+		if ($args->has('id')) {
+			if (is_array($availability) && $availability['update'] === true) {
+				$updateModel = new ResolveNested(new UpdateModel(new SaveModel($this->relation)));
 
-        if ($args->has('connect')) {
-            $this->relation->associate($args->arguments['connect']->value);
-        }
+				$related = $updateModel(
+					$this->relation->make(),
+					$args
+				);
+				$this->relation->associate($related);
+			} else {
+				$this->relation->associate($args->arguments['id']->value);
+			}
+		} else {
+			$this->relation->dissociate();
+			// If allowed delete current association
+			if (is_array($availability) && $availability['delete'] === true) {
+				$this->relation->delete();
+			}
 
-        if ($args->has('update')) {
-            $updateModel = new ResolveNested(new UpdateModel(new SaveModel($this->relation)));
+			if (is_array($availability) && $availability['create'] === true) {
+				$saveModel = new ResolveNested(new SaveModel($this->relation));
 
-            $related = $updateModel(
-                $this->relation->make(),
-                $args->arguments['update']->value
-            );
-            $this->relation->associate($related);
-        }
-
-        if ($args->has('upsert')) {
-            $upsertModel = new ResolveNested(new UpsertModel(new SaveModel($this->relation)));
-
-            $related = $upsertModel(
-                $this->relation->make(),
-                $args->arguments['upsert']->value
-            );
-            $this->relation->associate($related);
-        }
-
-        self::disconnectOrDelete($this->relation, $args);
-    }
-
-    public static function disconnectOrDelete(BelongsTo $relation, ArgumentSet $args): void
-    {
-        // We proceed with disconnecting/deleting only if the given $values is truthy.
-        // There is no other information to be passed when issuing those operations,
-        // but GraphQL forces us to pass some value. It would be unintuitive for
-        // the end user if the given value had no effect on the execution.
-        if (
-            $args->has('disconnect')
-            && $args->arguments['disconnect']->value
-        ) {
-            $relation->dissociate();
-        }
-
-        if (
-            $args->has('delete')
-            && $args->arguments['delete']->value
-        ) {
-            $relation->dissociate();
-            $relation->delete();
-        }
-    }
+				$related = $saveModel(
+					$this->relation->make(),
+					$args
+				);
+				$this->relation->associate($related);
+			}
+		}
+	}
 }
--- src/Execution/Arguments/NestedManyToMany.php
+++ src/Execution/Arguments/NestedManyToMany.php
@@ -3,98 +3,101 @@
 namespace Nuwave\Lighthouse\Execution\Arguments;
 
 use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 use Nuwave\Lighthouse\Support\Contracts\ArgResolver;
 
 class NestedManyToMany implements ArgResolver
 {
-    /**
-     * @var string
-     */
-    private $relationName;
+	/**
+	 * @var string
+	 */
+	private $relationName;
 
-    public function __construct(string $relationName)
-    {
-        $this->relationName = $relationName;
-    }
+	public function __construct(string $relationName)
+	{
+		$this->relationName = $relationName;
+	}
 
-    /**
-     * @param  \Illuminate\Database\Eloquent\Model  $parent
-     * @param  \Nuwave\Lighthouse\Execution\Arguments\ArgumentSet  $args
-     * @return void
-     */
-    public function __invoke($parent, $args)
-    {
-        /** @var \Illuminate\Database\Eloquent\Relations\BelongsToMany|\Illuminate\Database\Eloquent\Relations\MorphToMany $relation */
-        $relation = $parent->{$this->relationName}();
+	/**
+	 * @param  \Illuminate\Database\Eloquent\Model  $parent
+	 * @param  \Nuwave\Lighthouse\Execution\Arguments\ArgumentSet  $args
+	 * @return void
+	 */
+	public function __invoke($parent, $args)
+	{
+		/** @var \Illuminate\Database\Eloquent\Relations\BelongsToMany|\Illuminate\Database\Eloquent\Relations\MorphToMany $relation */
+		$relation = $parent->{$this->relationName}();
 
-        if ($args->has('sync')) {
-            $relation->sync(
-                $this->generateRelationArray($args->arguments['sync'])
-            );
-        }
+		$reflection = new \ReflectionClass($relation->getRelated());
+		$availability = $reflection->getStaticPropertyValue('AVAILABILITY_IN_GRAPHQL');
 
-        if ($args->has('syncWithoutDetaching')) {
-            $relation->syncWithoutDetaching(
-                $this->generateRelationArray($args->arguments['syncWithoutDetaching'])
-            );
-        }
+		if (!is_array($availability) || $availability['create'] === false) {
+			$relation->sync($this->generateRelationArray($args));
+		} else {
+			$current_related = $parent->{$this->relationName};
+			$related_to_keep = [];
 
-        NestedOneToMany::createUpdateUpsert($args, $relation);
+			foreach ($args->arguments as $nested_operation) {
+				$array = $nested_operation->toPlain();
+				if (!isset($array['id'])) {
+					$saveModel = new ResolveNested(new SaveModel($relation));
+					$saveModel($relation->make(), $nested_operation->value);
+				} else {
+					$related_to_keep[] = $array['id'];
+					if (is_array($availability) && $availability['update'] === true) {
+						$updateModel = new ResolveNested(new UpdateModel(new SaveModel($relation)));
+						$updateModel($relation->make(), $nested_operation->value);
+					} else {
+						$relation->attach($array['id']);
+					}
+				}
+			}
 
-        if ($args->has('delete')) {
-            $ids = $args->arguments['delete']->toPlain();
+			foreach ($current_related as $related) {
+				if (!in_array($related->id, $related_to_keep, false)) {
+					$relation->detach($related->id);
+					if (is_array($availability) && $availability['delete'] === true) {
+						$related->cascadeDelete();
+					}
+				}
+			}
+		}
+	}
 
-            $relation->detach($ids);
-            $relation->getRelated()::destroy($ids);
-        }
+	/**
+	 * Generate an array for passing into sync, syncWithoutDetaching or connect method.
+	 *
+	 * Those functions natively have the capability of passing additional
+	 * data to store in the pivot table. That array expects passing the id's
+	 * as keys, so we transform the passed arguments to match that.
+	 *
+	 * @param  \Nuwave\Lighthouse\Execution\Arguments\ArgumentSet $args
+	 * @return mixed[]
+	 */
+	private function generateRelationArray(ArgumentSet $args): array
+	{
+		$values = $args->toArray();
 
-        if ($args->has('connect')) {
-            $relation->attach(
-                $this->generateRelationArray($args->arguments['connect'])
-            );
-        }
+		if (empty($values)) {
+			return [];
+		}
 
-        if ($args->has('disconnect')) {
-            $relation->detach(
-                $args->arguments['disconnect']->toPlain()
-            );
-        }
-    }
+		// Since GraphQL inputs are monomorphic, we can just look at the first
+		// given value and can deduce the value of all given args.
+		$exemplaryValue = $values[0];
 
-    /**
-     * Generate an array for passing into sync, syncWithoutDetaching or connect method.
-     *
-     * Those functions natively have the capability of passing additional
-     * data to store in the pivot table. That array expects passing the id's
-     * as keys, so we transform the passed arguments to match that.
-     *
-     * @param  \Nuwave\Lighthouse\Execution\Arguments\Argument $args
-     * @return mixed[]
-     */
-    private function generateRelationArray(Argument $args): array
-    {
-        $values = $args->toPlain();
+		// We assume that the values contain pivot information
+		if (is_array($exemplaryValue)) {
+			$relationArray = [];
+			foreach ($values as $value) {
+				$id = Arr::pull($value, 'id');
+				$relationArray[$id] = $value;
+			}
 
-        if (empty($values)) {
-            return [];
-        }
+			return $relationArray;
+		}
 
-        // Since GraphQL inputs are monomorphic, we can just look at the first
-        // given value and can deduce the value of all given args.
-        $exemplaryValue = $values[0];
-
-        // We assume that the values contain pivot information
-        if (is_array($exemplaryValue)) {
-            $relationArray = [];
-            foreach ($values as $value) {
-                $id = Arr::pull($value, 'id');
-                $relationArray[$id] = $value;
-            }
-
-            return $relationArray;
-        }
-
-        // The default case is simply a flat array of IDs which we don't have to transform
-        return $values;
-    }
+		// The default case is simply a flat array of IDs which we don't have to transform
+		return $values;
+	}
 }
